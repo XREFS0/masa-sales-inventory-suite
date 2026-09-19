9eb9e416538543864ed37dce6d670a8663ecf29e
XREFS0 <xrefs0@users.noreply.github.com>
2026-09-19 17:09:19 +0200

    feat: initial release of masa-sales-inventory-suite by MASA

---
+++ Sales and Inventory System Project in Python/actions/__init__.py
@@ -0,0 +1,10 @@
+"""
+Developed by MASA
+All Rights Reserved.
+"""
+
+from .register_sale import *
+from .register_product_arrival import *
+from .query_inventory_data import *
+from .most_sold_items import *
+from .employees_with_most_sales import *
---
+++ Sales and Inventory System Project in Python/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Developed by MASA
+All Rights Reserved.
+"""
+
+from .products import *
+from .employees import *
+from .sales import *
+from .other import *
